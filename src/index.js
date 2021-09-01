ae93c08bfd437f557f4dc52ac1472020ad10285b
github username <sharnellgrant@gmail.com>
2021-09-01 00:15:21 -0400

    woring on form handler or form inputs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+const getReviewForm = document.getElementById('create-review-form')
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
    console.log("Document loaded")
    getReviews();
+   getReviewForm.addEventListener("submit", (e) => 
+   formEventHandler(e))
 })
 
 function getReviews(){
@@ -10,15 +16,40 @@ function getReviews(){
        info.data.forEach(review => {
            const reviewMarkup = `
               <div data-id=${review.id}>
-              <br> <h2>Name: ${review.name}</h2> </br>
-              <br> <img src=${review.image_url} height="200" width="250"</p> </br>
-              <br> <h3> Location: ${review.location}</h3> </br>
-              <br> <p> Feedback: ${review.feedback} </p> <br>
-              <br> <h4> Star Rating: </h4>
+              <br><h3>${review.attributes.first_name}</h3> <h3>${review.attributes.last_name}</h3></br>
+              <br> <h2>Name: ${review.attributes.name}</h2> </br>
+              <br> <img src=${review.attributes.image_url} height="200" width="250"</p> </br>
+              <br> <h3> Location: ${review.attributes.location}</h3> </br>
+              <section> <span> Feedback: ${review.attributes.feedback} </feedback> 
+              <br> <h4> Star Rating: ${review.attributes.stars}</h4>
              <button data-id=${review.id} type="button" class="delete-button">Delete</button>
-            </div>   `
-           
+             </div>   
+           `
+           document.getElementById("root").innerHTML += reviewMarkup
        })
     })
     
 }
+
+//this function is created to handle the events of the form 
+//grabbing all the values for my form input
+function formEventHandler(e){
+    e.preventDefault();
+    const firstNameInput = document.querySelector("#input-first-name").value
+    const lastNameInput = document.querySelector('#input-last-name').value
+    const nameInput = document.querySelector('#input-name').value
+    const imageInput = document.querySelector('#input-image').value
+    const locationInput = document.querySelector('#input-location').value
+    const feedbackInput = document.querySelector('#input-feedback').value
+    const starRatingInput = document.querySelector('#star-rating').value
+    const 
+}
+
+
+function postReviews(){
+  
+
+}
+
+
+
