08025602da1d2ac76eb87d6ca6af82a564277efe
github username <sharnellgrant@gmail.com>
2021-09-03 20:44:37 -0400

    completed post fetch request

--- src/index.js
+++ src/index.js
@@ -10,9 +10,10 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 function getReviews(){
-    fetch("http://[::1]:3000/api/v1/reviews")
+  fetch("http://localhost:3000/api/v1/reviews")
     .then(response => response.json())
-    .then(info => {
+    .then(info => { 
+      
        info.data.forEach(review => {
            const reviewMarkup = `
               <div data-id=${review.id}>
@@ -21,10 +22,12 @@ function getReviews(){
               <br> <img src=${review.attributes.image_url} height="200" width="250"</p> </br>
               <br> <h3> Location: ${review.attributes.location}</h3> </br>
               <section> <span> Feedback: ${review.attributes.feedback} </feedback> 
-              <br> <h4> Star Rating: ${review.attributes.stars}</h4>
+              <br> <h4> Star Rating: ${review.attributes.stars}</h4> </br>
+              <br> <p> Category: </p> </br>
              <button data-id=${review.id} type="button" class="delete-button">Delete</button>
              </div>   
            `
+        
            document.getElementById("root").innerHTML += reviewMarkup
        })
     })
@@ -42,12 +45,42 @@ function formEventHandler(e){
     const locationInput = document.querySelector('#input-location').value
     const feedbackInput = document.querySelector('#input-feedback').value
     const starRatingInput = document.querySelector('#star-rating').value
-    const 
+    //const categoryInput = document.querySelector('#categories').value
+    const categoryId = parseInt(document.querySelector('#categories').value)
+    postFetchReviews(firstNameInput, lastNameInput, nameInput, imageInput, locationInput, feedbackInput, starRatingInput, categoryId)
 }
 
 
-function postReviews(){
-  
+function postFetchReviews(first_name, last_name, name, image_url, location, feedback, stars, category_id){
+    
+  let formData = {first_name, last_name, name, image_url, location, feedback, stars, category_id}
+    
+  fetch("http://localhost:3000/api/v1/reviews", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+          body: JSON.stringify({review: formData})
+          })
+            .then(response => response.json())
+            .then(review => {
+              console.log(review)
+              const reviewData = review.data
+              const reviewMarkup = `
+              <div data-id=${review.id}>
+              <br><h3>${reviewData.attributes.first_name}</h3> <h3>${reviewData.attributes.last_name}</h3></br>
+              <br> <h2>Name: ${reviewData.attributes.name}</h2> </br>
+              <br> <img src=${reviewData.attributes.image_url} height="200" width="250"</p> </br>
+              <br> <h3> Location: ${reviewData.attributes.location}</h3> </br>
+              <section> <span> Feedback: ${reviewData.attributes.feedback} </feedback> 
+              <br> <h4> Star Rating: ${reviewData.attributes.stars}</h4> </br>
+              <br> <p> Category: </p> </br>
+             <button data-id=${reviewData.id} type="button" class="delete-button">Delete</button>
+             </div> 
+              `
+              document.getElementById("root").innerHTML += reviewMarkup
+    })
 
 }
 
