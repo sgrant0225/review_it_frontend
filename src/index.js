06439612508c5ae9fc2db41c354f5cbb1e71619c
github username <sharnellgrant@gmail.com>
2021-09-05 23:12:54 -0400

    Completed OOJS refactor and added delete funtionality

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 const getReviewForm = document.getElementById('create-review-form')
 
-
-
 document.addEventListener("DOMContentLoaded", () => {
    console.log("Document loaded")
    getReviews();
@@ -15,24 +13,17 @@ function getReviews(){
     .then(info => { 
       
        info.data.forEach(review => {
-           const reviewMarkup = `
-              <div data-id=${review.id}>
-              <br><h3>${review.attributes.first_name}</h3> <h3>${review.attributes.last_name}</h3></br>
-              <br> <h2>Name: ${review.attributes.name}</h2> </br>
-              <br> <img src=${review.attributes.image_url} height="200" width="250"</p> </br>
-              <br> <h3> Location: ${review.attributes.location}</h3> </br>
-              <section> <span> Feedback: ${review.attributes.feedback} </feedback> 
-              <br> <h4> Star Rating: ${review.attributes.stars}</h4> </br>
-              <br> <p> Category: </p> </br>
-             <button data-id=${review.id} type="button" class="delete-button">Delete</button>
-             </div>   
-           `
-        
-           document.getElementById("root").innerHTML += reviewMarkup
+        //debugger  
+        let newReview = new Review(review, review.attributes)
+        document.getElementById("root").innerHTML += newReview.renderReviewCard();
        })
-    })
+       const deleteEvent = document.querySelectorAll(".delete-button")
+        .forEach((button) => button.addEventListener("click", deleteReview))
+     })
     
-}
+  }
+
+  
 
 //this function is created to handle the events of the form 
 //grabbing all the values for my form input
@@ -40,22 +31,21 @@ function formEventHandler(e){
     e.preventDefault();
     const firstNameInput = document.querySelector("#input-first-name").value
     const lastNameInput = document.querySelector('#input-last-name').value
-    const nameInput = document.querySelector('#input-name').value
+    const productNameInput = document.querySelector('#input-name').value
     const imageInput = document.querySelector('#input-image').value
     const locationInput = document.querySelector('#input-location').value
     const feedbackInput = document.querySelector('#input-feedback').value
     const starRatingInput = document.querySelector('#star-rating').value
     //const categoryInput = document.querySelector('#categories').value
     const categoryId = parseInt(document.querySelector('#categories').value)
-    postFetchReviews(firstNameInput, lastNameInput, nameInput, imageInput, locationInput, feedbackInput, starRatingInput, categoryId)
+    postFetchReviews(firstNameInput, lastNameInput, productNameInput, imageInput, locationInput, feedbackInput, starRatingInput, categoryId)
+    e.target.reset();
 }
 
 
-function postFetchReviews(first_name, last_name, name, image_url, location, feedback, stars, category_id){
-    
-  let formData = {first_name, last_name, name, image_url, location, feedback, stars, category_id}
-    
-  fetch("http://localhost:3000/api/v1/reviews", {
+function postFetchReviews(first_name, last_name, product_name, image_url, location, feedback, stars, category_id){
+    let formData = {first_name, last_name, product_name, image_url, location, feedback, stars, category_id}
+    fetch("http://localhost:3000/api/v1/reviews", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -67,22 +57,23 @@ function postFetchReviews(first_name, last_name, name, image_url, location, feed
             .then(review => {
               console.log(review)
               const reviewData = review.data
-              const reviewMarkup = `
-              <div data-id=${review.id}>
-              <br><h3>${reviewData.attributes.first_name}</h3> <h3>${reviewData.attributes.last_name}</h3></br>
-              <br> <h2>Name: ${reviewData.attributes.name}</h2> </br>
-              <br> <img src=${reviewData.attributes.image_url} height="200" width="250"</p> </br>
-              <br> <h3> Location: ${reviewData.attributes.location}</h3> </br>
-              <section> <span> Feedback: ${reviewData.attributes.feedback} </feedback> 
-              <br> <h4> Star Rating: ${reviewData.attributes.stars}</h4> </br>
-              <br> <p> Category: </p> </br>
-             <button data-id=${reviewData.id} type="button" class="delete-button">Delete</button>
-             </div> 
-              `
-              document.getElementById("root").innerHTML += reviewMarkup
+              document.getElementById("root").innerHTML += reviewData.renderReviewCard();
     })
+  }
 
-}
-
-
+  function deleteReview(e) {
+     const id = e.target.dataset.id //target the delete button id
+    fetch(`http://localhost:3000/api/v1/reviews/${id}`, {
+      method: "DELETE", 
+     })
+     .then(response => response.json())
+     .then(deleteData => {
+      Review.all = Review.all.filter(review => review.id != deleteData.id)
+            
+      document.getElementById('root').innerHTML = "";
+        Review.all.forEach(filteredReview => {
+          document.getElementById('root').innerHTML += filteredReview.renderReviewCard();
+        })
+     })
+  }
 
--- src/review.js
+++ src/review.js
@@ -1,5 +1,41 @@
 class Review {
+  
+    constructor(review, reviewAttributes) {
+    this.id = review.id
+    this.first_name = reviewAttributes.first_name
+    this.last_name = reviewAttributes.last_name 
+    this.product_name = reviewAttributes.product_name 
+    this.image_url = reviewAttributes.image_url 
+    this.location = reviewAttributes.location 
+    this.feedback = reviewAttributes.feedback 
+    this.stars = reviewAttributes.stars 
+    this.category = reviewAttributes.category
+    //debugger
+    Review.all.push(this)
+  }
 
-  constructor
+   renderReviewCard(){
+    return `<div data-id=${this.id}>
+        <br><h3>${this.first_name} ${this.last_name}</h3></br>
+        <br> <h2> ${this.product_name}</h2> </br>
+        <br> <img src=${this.image_url} height="200" width="250"</p> </br>
+        <br> <h3> Location: ${this.location}</h3> </br>
+        <section> <span> Feedback: ${this.feedback} </feedback> 
+        <br> <h4> Star Rating: ${this.stars}</h4> </br>
+        <br> <p> Category: ${this.category.name} </p> </br>
+        <button data-id=${this.id} type="button" class="delete-button">Delete</button>
+      </div>  
+      `
+   }
+     
+    
+
 
 }
+
+
+  
+
+
+
+Review.all = [];
