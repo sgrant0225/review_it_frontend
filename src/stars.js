972915657473715755b672993a639fdb1b1128a9
github username <sharnellgrant@gmail.com>
2021-09-08 14:03:28 -0400

    Added star rating functionality part 2

--- src/stars.js
+++ src/stars.js
@@ -1,6 +1,10 @@
 
 const starWrapper = document.querySelector(".stars-container")
 const stars = document.querySelectorAll(".stars-container a");
+// const data = {
+//   starRating: 0,
+//   ratings: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }]
+// };
 
     stars.forEach((star, clickedIndex) => {
         star.addEventListener("click", () => {
@@ -10,14 +14,11 @@ const stars = document.querySelectorAll(".stars-container a");
               if (otherIndex <= clickedIndex) {
                   otherStar.classList.add("active");
               }
-            });
+            }); 
             console.log(`star of index ${clickedIndex + 1} was clicked`)
 
             //post to backend the star rating 
-            const data = {
-                starRating: 0,
-                ratings: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }]
-              };
+            
         })
      });
 
